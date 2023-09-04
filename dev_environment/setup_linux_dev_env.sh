d6a9e3f72155070582959f71bc19bd3592cb3117
Atul-source <atulprajapati6031@gmail.com>
2023-09-04 11:17:08 +0530

    removed unwanted changes
    Signed-off-by: Atul-source <atulprajapati6031@gmail.com>

--- dev_environment/setup_linux_dev_env.sh
+++ dev_environment/setup_linux_dev_env.sh
@@ -73,14 +73,14 @@ esac
 cd /root
 
 # install packages
-#apt-get update
-#apt-get install -y software-properties-common wget
+apt-get update
+apt-get install -y software-properties-common wget
 
 # get grafana package
-#wget -q -O - https://packages.grafana.com/gpg.key | apt-key add -
-#echo "deb https://packages.grafana.com/oss/deb stable main" | tee -a /etc/apt/sources.list.d/grafana.list
-#apt-get clean
-#apt-get update
+wget -q -O - https://packages.grafana.com/gpg.key | apt-key add -
+echo "deb https://packages.grafana.com/oss/deb stable main" | tee -a /etc/apt/sources.list.d/grafana.list
+apt-get clean
+apt-get update
 # -end-
 
 # install all necessary packages
