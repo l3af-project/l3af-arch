99935bda936623c554320730bdbdc22c083003ba
Atul-source <atulprajapati6031@gmail.com>
2023-09-28 21:40:06 +0530

    testing workflow
    Signed-off-by: Atul-source <atulprajapati6031@gmail.com>

---
+++ dev_environment/e2e_test/start_test_servers.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -eux
+
+# Host the build artifacts with an HTTP server on port 8000
+BUILD_ARTIFACT_DIR=/srv/l3afd
+
+# If BUILD_ARTIFACT_DIR then the provisiniong script hasn't run yet and we
+# don't need to start the test servers
+if [ ! -d $BUILD_ARTIFACT_DIR ]
+then
+	exit 0
+fi
+
+cd $BUILD_ARTIFACT_DIR
+python3 -m http.server 8000 > httpserver.log 2>&1 &
+
+# Start the test web servers
+/usr/local/go/bin/go run /root/l3af-arch/dev_environment/code/web-server.go -port 8080 > server1.log 2>&1 &
+/usr/local/go/bin/go run /root/l3af-arch/dev_environment/code/web-server.go -port 8081 > server2.log 2>&1 &
