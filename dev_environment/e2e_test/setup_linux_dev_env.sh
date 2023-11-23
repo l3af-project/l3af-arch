ee2af75e8876cd92bef3a01813d33bf1e06fee53
Atul-source <atulprajapati6031@gmail.com>
2023-11-23 22:13:42 +0530

    removed local refs
    Signed-off-by: Atul-source <atulprajapati6031@gmail.com>

--- dev_environment/e2e_test/setup_linux_dev_env.sh
+++ dev_environment/e2e_test/setup_linux_dev_env.sh
@@ -121,7 +121,7 @@ apt-get install -y bc \
 
 if [ ! -d "/root/l3af-arch" ];
 then
-  git clone -b vm-test https://github.com/Atul-source/l3af-arch.git
+  git clone https://github.com/l3af-project/l3af-arch.git
 else
   echo "/root/l3af-arch directory already exists"
 fi
