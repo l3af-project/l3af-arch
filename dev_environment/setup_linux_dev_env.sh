f5350135fd9092f7d462bdc9a841b8b6d1dda0b4
Atul-source <atulprajapati6031@gmail.com>
2024-06-12 11:06:46 +0530

    Combined test coverage result for unit tests and integrations tests
    Signed-off-by: Atul-source <atulprajapati6031@gmail.com>

--- dev_environment/setup_linux_dev_env.sh
+++ dev_environment/setup_linux_dev_env.sh
@@ -121,7 +121,16 @@ apt-get install -y bc \
   wget https://go.dev/dl/$go_filename
   tar -C /usr/local -xzf $go_filename && rm -f $go_filename
   export PATH=$PATH:/usr/local/go/bin
+  export GOCOVERDIR="/root/coverdata/int"
+  echo export PATH=$PATH:/root/coverdata/int >> /root/.bashrc
+
+# Test coverdata
+  mkdir /root/coverdata
+  mkdir /root/coverdata/int
+  mkdir /root/coverdata/unit
+  mkdir /root/coverdata/combined
   echo export PATH=$PATH:/usr/local/go/bin >> /root/.bashrc
+  source /root/.bashrc
 
 # Clone the l3afd repo in to root directly
 # Can use mapped directory i.e. at /home/ubuntu/Home
@@ -250,3 +259,4 @@ else
   /root/l3af-arch/dev_environment/start_test_servers.sh
   ./l3afd --config /root/l3af-arch/dev_environment/cfg/l3afd.cfg &
 fi
+
