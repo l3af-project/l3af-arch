469986c82644f561dae55c9dc279b0f0dee9e170
Atul-source <atulprajapati6031@gmail.com>
2024-07-11 10:24:40 +0530

    addressed comments
    Signed-off-by: Atul-source <atulprajapati6031@gmail.com>

--- dev_environment/generate_payload.sh
+++ dev_environment/generate_payload.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-
 if [ $# -ge 1 ] && [ "$1" == "-i" ]; then
 	shift;
 	echo "This script will change only hostname & interface name if you want more sophisticated configuration Please change payload files manually"
@@ -9,7 +8,6 @@ if [ $# -ge 1 ] && [ "$1" == "-i" ]; then
 	find ./e2e_test -type f -name "*.json" -exec sed -i "s/l3af-test-host/$hm/g" {} +
 	find ./cfg -type f -name "*.json" -exec sed -i "s/enp0s3/$1/g" {} +
 	find ./e2e_test -type f -name "*.json" -exec sed -i "s/ibpfbr/$1/g" {} +
-	#do your stuff 
 else
 	echo "To run the script: "
 	echo "      ./generate_payload.sh -i <interface_name> "	
